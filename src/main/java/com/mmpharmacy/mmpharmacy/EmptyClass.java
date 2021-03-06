227974063c0300e4e000953321f5c26c31298d08
Mubariz Azimov <mubariz.azim.09@gmail.com>
2021-03-06 23:19:48 +0100

    hello world by mubu OKKKK

--- src/main/java/com/mmpharmacy/mmpharmacy/EmptyClass.java
+++ src/main/java/com/mmpharmacy/mmpharmacy/EmptyClass.java
@@ -6,5 +6,6 @@ public static   void main(String[] args){
     System.out.println("Hello World");
     System.out.println("Hello World by Mubu");
     System.out.println("Hello World by Murad");
+    System.out.println("ok");
 }
 }
