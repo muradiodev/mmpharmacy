8207f7c80b77fee55fc6b7f9546e3d40e6197733
muradjava <muradace@gmail.com>
2021-03-07 00:07:36 +0100

    added entity Time and updated app.properties and fixed bug

--- src/main/java/com/mmpharmacy/mmpharmacy/MmpharmacyApplication.java
+++ src/main/java/com/mmpharmacy/mmpharmacy/MmpharmacyApplication.java
@@ -2,8 +2,13 @@ package com.mmpharmacy.mmpharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
 public class MmpharmacyApplication {
 
     public static void main(String[] args) {
