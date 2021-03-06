a74268dd829602129671cf0dfe9b39cfa0a12da6
muradjava <muradace@gmail.com>
2021-03-06 23:24:51 +0100

    added "Hello world" by Murad

--- src/main/java/com/mmpharmacy/mmpharmacy/EmptyClass.java
+++ src/main/java/com/mmpharmacy/mmpharmacy/EmptyClass.java
@@ -2,9 +2,9 @@ package com.mmpharmacy.mmpharmacy;
 
 public class EmptyClass {
 
-public static   void main(String[] args){
-    System.out.println("Hello World");
-    System.out.println("Hello World by Mubu");
-    System.out.println("Hello World by Murad");
-}
+//public static   void main(String[] args){
+//    System.out.println("Hello World");
+//    System.out.println("Hello World by Mubu");
+//    System.out.println("Hello World by Murad");
+//}
 }
