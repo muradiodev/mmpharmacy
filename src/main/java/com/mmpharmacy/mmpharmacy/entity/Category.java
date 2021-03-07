6d927fca5170aac0746946838c16b3acc08e8926
Mubariz Azimov <mubariz.azim.09@gmail.com>
2021-03-07 02:13:06 +0100

    location get out

---
+++ src/main/java/com/mmpharmacy/mmpharmacy/entity/Category.java
@@ -0,0 +1,42 @@
+package com.mmpharmacy.mmpharmacy.entity;
+
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+@ToString
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/mmpharmacy/mmpharmacy/entity/Type.java
@@ -0,0 +1,42 @@
+package com.mmpharmacy.mmpharmacy.entity;
+
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "type")
+@ToString
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Type() {
+    }
+
+    public Type(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
