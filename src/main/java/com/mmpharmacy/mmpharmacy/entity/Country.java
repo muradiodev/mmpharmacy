118f5a0a2abe6d540741b156d6681eb8ba55880e
Mubariz Azimov <mubariz.azim.09@gmail.com>
2021-03-07 01:45:39 +0100

    DB Country Entity completed by Mubariz

---
+++ src/main/java/com/mmpharmacy/mmpharmacy/entity/Country.java
@@ -0,0 +1,42 @@
+package com.mmpharmacy.mmpharmacy.entity;
+
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="country")
+@ToString
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
